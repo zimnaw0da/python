df6f7f90f40d87ab0ee28d4686388796f0d4a92b
defvvl <129220387+bashvvl@users.noreply.github.com>
2023-09-26 20:49:21 +0200

    Update data_table.py

--- data_table.py
+++ data_table.py
@@ -6,5 +6,7 @@ Table.add_row(["Admin", "Admin123", "Undfined", "24.09.2023",])
 Table.add_row(["Kali", "Kali#1731", "Undfined", "24.09.2023",])
 Table.add_row(["vvl", "wdfihb#208", "Undfined", "24.09.2023",])
 Table.add_row(["critick", "heartattack$bash", "Undfined", "24.09.2023",])
+Table.add_row(["cow", "coweed#17463y", "Undfined", "26.09.2023",])
+
 
 print(Table)
