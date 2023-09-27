8861f92a7e6ab8feaf2a183354b33f96d4e8ae5e
defvvl <marcelchoroszucha@gmail.com>
2023-09-27 16:06:32 +0200

    Update data_table.py

--- data_table.py
+++ data_table.py
@@ -7,6 +7,6 @@ Table.add_row(["Kali", "Kali#1731", "Undfined", "24.09.2023",])
 Table.add_row(["vvl", "wdfihb#208", "Undfined", "24.09.2023",])
 Table.add_row(["critick", "heartattack$bash", "Undfined", "24.09.2023",])
 Table.add_row(["cow", "coweed#17463y", "Undfined", "26.09.2023",])
-
+Table.add_row(["Commet", "astres20i@E01sk", "Undfined", "27.09.2023",])
 
 print(Table)
